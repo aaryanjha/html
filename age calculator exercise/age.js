bbb31132a59586a9474ad50119cfde9e8393eab9
aaryanjha <47775122+aaryanjha@users.noreply.github.com>
2019-10-21 01:03:56 +0530

    Add files via upload

---
+++ age calculator exercise/age.js
@@ -0,0 +1,3 @@
+var age=prompt("Enter your age in years: ");
+var days = age * 365.25;
+console.log("Total no. of days you live is "+days+".")
---
+++ stalker exercise/stalk.js
@@ -0,0 +1,5 @@
+var first=prompt("Enter the user's first name: ");
+var last=prompt("Enter the user's last name: ");
+var age=prompt("Enter the user's age: ");
+console.log("The full name of user is " + first + " " + last+".");
+console.log("And his age is "+ age + " years.");
